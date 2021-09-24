4238073f533351469d63e6565963e06fdf210ae7
Michael Harfenist <hrharfenist@gmail.com>
2021-09-24 13:16:42 -0400

    tests are difficult

--- my.test.js
+++ my.test.js
@@ -1,17 +1,37 @@
+/**
+ * @jest-environment jsdom
+ */
+
+
 // test('adds 1 + 2 to equal 3', () => {
 //     expect(1+2).toBe(3);
 //   });
 
 
+
 import React from 'react';
 
-import {state} from './client/components/RelatedPoductsAndOutfit.jsx'
-import {handleRelatedProductsClick} from './client/components/App.js'
+import App from './client/components/App.js'
+import Adapter from 'enzyme-adapter-react-16';
+import Enzyme, {shallow} from 'enzyme'
+import RelatedProducts from './client/components/RelatedProductsAndOutfits/RelatedProducts.jsx'
+Enzyme.configure({ adapter: new Adapter() })
 
 
-test('Updates related products when rendering page', () => {
-  productId = '37313';
-  handleRelatedProductsClick(productId)
-  expect(state.RelatedProducts).toContain('37315')
-
+test('Renders Correctly', () => {
+  const app = shallow(<App currentProductId='37313'/>)
+  //console.log(app.currentProductId)
+  expect(app).toMatchSnapshot();
 })
+
+
+// const clickFn = jest.fn();
+
+// test('Re-renders App when clicking on related product correctly', () => {
+//   const card = shallow(<RelatedProducts  onClick={clickFn}/>)
+//   var nextButton = card.find('.carousal-next')
+//   console.log(nextButton)
+//   nextButton.simulate('click');
+
+//   expect(clickFn).toHaveBeenCalled();
+// })
